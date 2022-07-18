d12df8643779f1d1e7cddbc01b126930209ac9dc
Augusto Marques <augustomarqq@gmail.com>
2022-07-18 20:24:49 -0300

    6th exercise module 3

--- softexBack/logica/modulo3/exe6.py
+++ softexBack/logica/modulo3/exe6.py
@@ -1,13 +1,15 @@
 """Atividade 6 - Adivinha Idade"""
 
 while True:
-    nome = input("Digite o seu nome completo: ")
-    anoNasc = int(input("Em qual ano você nasceu? "))
-    idade = 2022 - anoNasc
+    try:        
+        nome = input("Digite o seu nome completo: ")
+        anoNasc = int(input("Em qual ano você nasceu? "))
+        idade = 2022 - anoNasc
 
-    if anoNasc >= 1922 and anoNasc <= 2021:
-        print (f"O usuário {nome} vai completar {idade} anos em 2022")
-        break
-    else:
+        if anoNasc >= 1922 and anoNasc <= 2021:
+            print (f"O usuário {nome} vai completar {idade} anos em 2022")
+            break
+        else:
+            print("Dados inválidos, por favor preencha novamente")
+    except:
         print("Dados inválidos, por favor preencha novamente")
-        
