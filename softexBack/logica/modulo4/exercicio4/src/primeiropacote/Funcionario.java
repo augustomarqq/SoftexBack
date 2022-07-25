dfed90c8fa41646776a54bfa10b99daec3532f1e
Augusto Marques <augustomarqq@gmail.com>
2022-07-25 01:45:14 -0300

    4rd exercise module 4

---
+++ softexBack/logica/modulo4/exercicio4/src/primeiropacote/Funcionario.java
@@ -0,0 +1,11 @@
+package primeiropacote;
+
+public class Funcionario {    
+        String nome;
+        String funcao;
+
+        public Funcionario(String nome, String funcao){
+            this.nome = nome;
+            this.funcao = funcao;
+        }
+    }
---
+++ softexBack/logica/modulo4/exercicio4/src/segundopacote/Cliente.java
@@ -0,0 +1,11 @@
+package segundopacote;
+
+public class Cliente {    
+        String nome;
+        String nivel;
+
+        public Cliente(String nome, String nivel){
+            this.nome = nome;
+            this.nivel = nivel;
+        }
+    }
