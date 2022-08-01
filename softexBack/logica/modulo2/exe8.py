1b89144b61c57899eae5a77dd1587db805f67f43
Augusto Marques <augustomarqq@gmail.com>
2022-07-31 23:22:29 -0300

    8th exercise module 2

--- softexBack/logica/modulo2/exe8.py
+++ softexBack/logica/modulo2/exe8.py
@@ -13,12 +13,12 @@ def resultadoFinal(dataframe):
     dataframe.loc[(dataframe["media"] >= 7) & (dataframe["faltas"] < 5),"situacao"] = "APROVADO!"
 
     print(dataframe)
-    maiorFalta = dataframe["faltas"].max()
+    maisFalta = dataframe["faltas"].max()
     maiorMedia =dataframe["media"].max()
     mediatotal = [10, 9, 8, 7, 6]
 
-    print("A média geral das notas da turma é: ",numpy.mean(mediatotal))
-    print("A maior quantidade de faltas é: ", maiorFalta)
+    print("A média geral das notas da turma é: ", numpy.mean(mediatotal))
+    print("A maior quantidade de faltas é: ", maisFalta)
     print("A maior média foi: ", maiorMedia)
     
 resultadoFinal(data)
