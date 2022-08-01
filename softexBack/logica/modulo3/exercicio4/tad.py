a028bcd17834f8e82a172aa30d99218242ea23e5
Augusto Marques <augustomarqq@gmail.com>
2022-07-31 22:57:31 -0300

    4th exercise module 3

---
+++ softexBack/logica/modulo3/exercicio4/tad.py
@@ -0,0 +1,31 @@
+x = complex(10,4)
+y = complex(2,3)
+z = complex(5,2)
+
+print("A classe de x é:", type(x))
+print("A parte real de x é =", x.real)
+print("A parte imaginária de x é =", x.imag)
+print("Logo, x =", x)
+
+print("\nA classe de y é:", type(y))
+print("A parte real de y é =", y.real)
+print("A parte imaginária de y é =", y.imag)
+print("Logo, y =", y)
+
+print("\nA classe de z é:", type(z))
+print("A parte real de z é =", z.real)
+print("A parte imaginária de z é =", z.imag)
+print("Logo, z =", z)
+
+#soma
+print("\n\nSeguem todas as operações básicas envolvendo os números complexos criados:")
+print("\nA soma de x, y, z = ", x+y+z)
+
+#subtração
+print("\nA subtração de x, y, z = ", x-y-z)
+
+#multiplicação
+print("\nA multiplicação de x, y, z = ", x*y*z)
+
+#divisão
+print("\nA divisão de x, y, z = ", x/y/z, "\n")
