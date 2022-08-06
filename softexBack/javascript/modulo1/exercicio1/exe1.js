fdc18d44ec4b597bc263fb5599fb9914d8f4db19
Augusto Marques <augustomarqq@gmail.com>
2022-08-06 11:57:34 -0300

    1st exercise module 1

---
+++ softexBack/javascript/modulo1/exercicio1/exe1.js
@@ -0,0 +1,16 @@
+// Este primeiro código abaixo funciona como botão de pesquisa do google.
+// Input que vai receber os dados que o usuário quer pesquisar. */
+
+<input class="gNO89b" value="Pesquisa Google" aria-label="Pesquisa Google" name="btnK" role="button" tabindex="0" type="submit" data-ved="0ahUKEwiZ4ITYs7L5AhXzu5UCHXTqAAcQ4dUDCA0"></input>
+
+
+// Este segundo código abaixo é o botão que vai submeter a inscrição do usuário à um determinado canal do Youtube.
+
+<tp-yt-paper-button noink="" class="style-scope ytd-subscribe-button-renderer" style-target="host" role="button" tabindex="0" animated="" elevation="0" aria-disabled="false" aria-label="Inscreva-se em Nautilus."><!--css-build:shady-->
+  <yt-formatted-string class="style-scope ytd-subscribe-button-renderer">Inscrever-se</yt-formatted-string>
+<paper-ripple class="style-scope tp-yt-paper-button"><!--css-build:shady-->
+    
+
+    <div id="background" class="style-scope paper-ripple"></div>
+    <div id="waves" class="style-scope paper-ripple"></div>
+</paper-ripple></tp-yt-paper-button>
