753abc702d34197aa83cede3b6e4d423d5761faa
Augusto Marques <augustomarqq@gmail.com>
2022-08-09 00:20:31 -0300

    2nd exercise module 2

--- softexBack/javascript/modulo2/exercicio2/app.js
+++ softexBack/javascript/modulo2/exercicio2/app.js
@@ -1,12 +1,13 @@
 import { somar, subtrair , multiplicar , dividir } from "./funcoes.js";
 import readlineSync from "readline-sync";
+const rl = readlineSync;
 
-var num1 = readlineSync.questionFloat("Valor 1: ");
-var num2 = readlineSync.questionFloat("Valor 2: ");
+var num1 = rl.questionFloat("Valor 1: ");
+var num2 = rl.questionFloat("Valor 2: ");
 
 while(true){
 
-    var op = readlineSync.question("Escolha a operacao a ser realizada:\n[+]\n[-]\n[*]\n[/]\n ");
+    var op = rl.question("Escolha a operacao a ser realizada:\n[+]\n[-]\n[*]\n[/]\n ");
 
     if (op == "+"){        
         somar(num1, num2);        
@@ -21,5 +22,5 @@ while(true){
         dividir(num1, num2);
         break;
     } else
-        console.log("Escolha uma operacao válida!\n")
+        console.log("Escolha uma operacao válida!\n");
 }
--- softexBack/javascript/modulo2/exercicio3/app.js
+++ softexBack/javascript/modulo2/exercicio3/app.js
@@ -1,9 +1,8 @@
-const readlineSync = require("readline-sync");
+import readlineSync from "readline-sync";
 
 var num1 = readlineSync.question("Digite o primeiro valor da operação: ");
 var num2 = readlineSync.question("Digite o segundo valor da operação: ");
-var res = parseFloat(n1) + parseFloat (n2);
-var op
+var res = parseFloat(num1) + parseFloat (num2);
 
-console.log(num1+ "+" +num2+ "=" +total);
+console.log(num1+ "+" +num2+ "=" +res);
 readlineSync.question("Aperte qualquer botão para sair");
