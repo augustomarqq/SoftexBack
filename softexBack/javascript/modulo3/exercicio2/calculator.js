a9bc0c5aa31508f4978a80a87f23f6d8ca45bcd0
Augusto Marques <augustomarqq@gmail.com>
2022-08-09 17:31:03 -0300

    2nd exercise module 3

---
+++ softexBack/javascript/modulo3/exercicio2/calculator.js
@@ -0,0 +1,3 @@
+import { calculadora } from "./functions.js";
+
+calculadora();
---
+++ softexBack/javascript/modulo3/exercicio2/functions.js
@@ -0,0 +1,47 @@
+import readlineSync from "readline-sync";
+const rl = readlineSync;
+
+const somar = (a, b) => a + b
+
+const subtrair = (a, b) => a - b
+
+const multiplicar = (a, b) => a * b
+
+const dividir = (a, b) => a / b
+
+function printaResto(a, b) {    
+    return console.log("O resultado da divisão entre "+ a +" e "+ b +" é = "+ (a / b).toFixed(2) +"\nE o resto é = "+ a%b)
+}
+
+export function calculadora() {
+
+var n1 = rl.questionFloat("Valor 1: ");
+var n2 = rl.questionFloat("Valor 2: ");
+
+while(true) {
+    
+    var op = rl.question("Escolha a operacao a ser realizada:\n[+]\n[-]\n[*]\n[/]\n ");
+
+    if (op == "+") {        
+        somar(n1, n2);
+        console.log("O resultado da soma entre "+ n1 +" e "+ n2 +" é = "+ (n1 + n2));
+        break;       
+    } else if (op == "-") {
+        subtrair(n1, n2);
+        console.log("O resultado da subtração entre "+ n1 +" e "+ n2 +" é = "+ (n1 - n2));
+        break;   
+    } else if (op == "*") {
+        multiplicar(n1, n2);
+        console.log("O resultado da multiplicação entre "+ n1 +" e "+ n2 +" é = "+ (n1 * n2));
+        break;
+    } else if (op == "/") {
+        dividir(n1, n2)
+        if (n1%n2 == 0)
+            console.log("O resultado da divisão entre "+ n1 +" e "+ n2 +" é = "+ (n1 / n2));
+        else
+            return printaResto(n1, n2);
+            break;
+    } else
+        console.log("Escolha uma operação válida!\n");
+};
+};
