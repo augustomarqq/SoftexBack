26332f0bb0b84e06fc7e62f1ac162f3b6675298a
Augusto Marques <augustomarqq@gmail.com>
2022-08-18 20:36:04 -0300

    2nd exercise module 4

--- softexBack/javascript/modulo4/exercicio1/banco.js
+++ softexBack/javascript/modulo4/exercicio1/banco.js
@@ -12,6 +12,7 @@ function Banco(conta, saldo, tipoConta, agencia) {
     }
 
     this.deposito = function(a) {
+       
         this.saldo += a
     }
 
@@ -23,20 +24,35 @@ function Banco(conta, saldo, tipoConta, agencia) {
         return this.conta;       
     }
 
+    this.valorAposDeposito = function(){
+        return this.saldo + this.deposito;
+        
+
+    }
+
+    this.valorAposSaque = function(){
+        return this.saldo - this.saque;
+    }
+
+    
+
 }
 
-var bancoInter = new Banco (undefined, undefined, undefined, undefined)
+var bancoInter = new Banco ()
 
 bancoInter.conta = rl.question("Digite o numero da sua conta: ")
 bancoInter.saldo = rl.questionFloat("Digite o valor do seu saldo: ")
 bancoInter.tipoConta = rl.question("Digite o tipo da sua conta: ")
 bancoInter.agencia = rl.question("Digite o numero da sua agencia: ")
 
+
 console.log("\nNumero da conta: "+ bancoInter.numConta())
 console.log("Tipo da conta: "+ bancoInter.tipoConta)
 console.log("Agencia: "+ bancoInter.agencia)
 console.log("Saldo: "+ bancoInter.saldo)
-bancoInter.deposito(2000)
-console.log("Saldo apos deposito de BRL2000: "+ bancoInter.saldoAtual())
-bancoInter.saque(1200)
-console.log("Saldo apos saque de BRL1200: "+ bancoInter.saldoAtual())
+bancoInter.deposito = rl.questionInt("Digite o valor do seu deposito: ")
+console.log("Deposito: R$"+ bancoInter.deposito)
+console.log("Saldo apos deposito: "+ bancoInter.valorAposDeposito())
+bancoInter.saque = rl.questionInt("Digite o valor do seu saque: ")
+console.log("Saque: R$"+ bancoInter.saque)
+console.log("Saldo apos saque: "+ bancoInter.valorAposSaque())
---
+++ softexBack/javascript/modulo4/exercicio2/http/app.js
@@ -0,0 +1,33 @@
+//import express from "express";
+
+var express = require('express');
+
+const app = express();
+
+app.listen(8080, () => {
+  console.log("Servidor de pé em http://localhost:8080")
+  console.log("Para desligar o servidor: ctrl + c")
+});
+
+// rotas
+app.get('/', (req ,res) =>{    
+    res.send("<h1>Home</h1>");
+});
+
+app.get('/user', (req, res) =>{
+    res.send(`
+        <h1>Usuário<h1>
+        
+        <form action="/user" method="POST">
+            <label for="email">E-mail:</label>
+            <input type="email" name="email" id="email">
+            <label for="mensagem">Mensagem:</label>
+            <textarea name="mensagem" id="mensagem"></textarea>
+            <input type="submit" value="Enviar">
+        </form>
+    `)
+});
+
+app.post('/user', (req, res) => {
+    res.send("<h1>Mensagem enviada com sucesso!</h1>");
+  });
