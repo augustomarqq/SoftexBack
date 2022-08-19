f1730c695e85759105754f9bef0967cc64390910
Augusto Marques <augustomarqq@gmail.com>
2022-08-18 21:12:32 -0300

    3rd exercise module 4

--- softexBack/javascript/modulo4/exercicio3/exe3.js
+++ softexBack/javascript/modulo4/exercicio3/exe3.js
@@ -1,14 +1,20 @@
 //Criando um objeto com no mínimo 3 propriedades e listando-as
 
 //Objeto
-var timesFutebol = {serieA:"palmeiras", serieB:"nautico", serieC:"remo", serieD:"santa cruz"};
+var timesFutebol = {
+    serieA:"palmeiras",
+    serieB:"nautico", 
+    serieC:"remo",
+    serieD:"santa cruz"
+};
 
 //Para prop (propriedade) in timesFutebol (objeto) faça
+let listaPropriedades = () => {
 for (var prop in timesFutebol) {
   // ctrl+shift+k (para abrir o console no mozilla firefox)
   console.log(prop + " = " + timesFutebol[prop]);
-}
-console.log("\n")
+};
+};
 
 
 //Criando um array com no mínimo 3 elementos e listando-os.
@@ -17,6 +23,12 @@ console.log("\n")
 let alunos = ["augusto", "guilherme", "camilla", "susy"];
 
 //Para nomes (elementos) in alunos (array) faça
+let listaElementos = () => {
 for (const nomes of alunos) {
-  console.log(nomes);
-}
+  console.log("aluno = " + nomes);
+};
+};
+
+listaPropriedades()
+console.log("\n")
+listaElementos()
